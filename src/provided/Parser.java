6f132277535ae5c1ecb16bf6b81b7f90dab585de
SeanB <srb6@rice.edu>
2016-01-22 17:25:40 -0600

    comp lab exp/factor parse sesh

--- src/provided/Parser.java
+++ src/provided/Parser.java
@@ -7,6 +7,8 @@ import provided.ParseException;
 import provided.Lexer;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 /** Each parser object in this class contains an embedded lexer which contains an embedded input stream.  The
   * class include a parse() method that will translate the program text in the input stream to the corresponding
@@ -15,7 +17,6 @@ import java.io.*;
 public class Parser {
   
   private Lexer in;
-  private AST result;
 
   //TODO
   
@@ -31,47 +32,13 @@ public class Parser {
   public Lexer lexer() { return in; }
   
   private void initParser() {
-	  result = null;
   }
   
   /** Parses the program text in the lexer bound to 'in' and returns the corresponding AST. 
     * @throws ParseException if a syntax error is encountered (including lexical errors). 
     */
   public AST parse() throws ParseException {
-	  Token token = in.readToken();
-	  //TokenType type = token.getType();
-	  if ((token instanceof BOOL) || (token instanceof INT) || (token instanceof NULL)) {
-		  result = parseTerm(token);
-	  } else if (token instanceof Op){
-		  Op op = (Op) token;
-		  if (op.isUnOp()){
-			  result = parseTerm(token);
-		  }
-	  } else if (token instanceof Map) {
-		  Map map = (Map) token;
-		  result = parseExp();		  
-	  } else if (token instanceof If) {
-		  If iff = (If) token;
-		  result = parseExp();	
-	  } else if (token instanceof Let) {
-		  Let let = (Let) token;
-		  result = parseExp();	
-	  } else if (token instanceof PrimFun) {
-      result = parseFactor();
-      else if (token instanceof Id) {
-      result = parseFactor(); 
-    } else if (token instanceof LeftParen) {
-      result = parseFactor();
-    } else if (token instanceof)
-
-    }
-
-    }
-	  
-	  }
-	  
-	  
-	return result;
+	return parseExp();
   }
   
 
@@ -109,15 +76,85 @@ public class Parser {
     * @return  the corresponding AST.
     */
   private AST parseExp() {
-	  return (AST) in.peek();
+	  Token token = in.readToken();
+	  //TokenType type = token.getType();
+	  AST result = null;
+	  if ((token instanceof BoolConstant) || (token instanceof IntConstant) || (token instanceof NullConstant)) {
+		  result = parseTerm(token);
+	  } else if (token instanceof Op){
+		  Op op = (Op) token;
+		  if (op.isUnOp()){
+			  result = parseTerm(token);
+		  }
+	  } else if (token instanceof Map) {
+		  token = in.readToken();
+		  if (token instanceof LeftBrace){
+			  token = in.readToken();
+			  ArrayList<Variable> vars = new ArrayList<Variable>();
+			  while (true){
+				  if (token instanceof Variable){
+					  Variable var = (Variable) token;
+					  vars.add(var);
+					  token = in.readToken();
+					  if (token instanceof Comma){
+						  token = in.readToken();
+					  } else {
+						  break;
+					  }
+				  } else {
+					  error(token,"map");
+				  }
+			  }
+			  if (token instanceof RightBrace){
+				  token = in.readToken();
+			  } else {
+				  error(token,"map");
+			  }
+			  if (token instanceof KeyWord){
+				  KeyWord word = (KeyWord) token;
+				  if (word.getName() == "to"){
+					  token = in.readToken();
+				  } else {
+					  error(token,"map");
+				  }
+			  } else {
+				  error(token,"map");
+			  }
+			  return new Map((Variable[])vars.toArray(),parseExp());
+		  }else {
+			  error(token,"map");
+		  }
+	  } else if (token instanceof If) {
+		  If iff = (If) token;
+		  //TODO
+	  } else if (token instanceof Let) {
+		  Let let = (Let) token;
+		  //TODO	
+	  } else if (token instanceof PrimFun) {
+		  result = parseFactor(token);
+	  } else if (token instanceof Variable) {
+	      result = parseFactor(token); 
+	  } else if (token instanceof LeftParen) {
+	      result = parseFactor(token);
+	  }
+	  return result;
   }
   
   private AST parseFactor(Token token) {
-	  TokenType type = token.getType();
-	  if (type == TokenType.OPERATOR){
-		  
-	  }
-	  if (token instanceof Term ){
+	  if (token instanceof LeftParen){
+		  AST exp = parseExp();
+		  token = in.readToken();
+		  if (token instanceof RightParen){
+			  return exp;
+		  } else{
+			  error(token,"factor paren");
+		  }
+	  } else if (token instanceof PrimFun) {
+		  return (PrimFun) token;
+	  } else if (token instanceof Variable) {
+		  return (Variable) token;
+	  } else{
+		  error(token,"unknown factor");
 	  }
 	  return (AST) in.peek();
 	  }
@@ -128,8 +165,8 @@ public class Parser {
 	  return out;
   }
   
-  private void error(Op op, String message){
-	  System.err.println(op.toString() + " caused an error: " + message);
+  private void error(Token token, String message){
+	  System.err.println(token.toString() + " caused an error: " + message);
   }
   
 
