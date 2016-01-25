51910f955bc5ba70796e96c537ac5e19e0af6aec
Xiao Zheng <yjzhengxiao@126.com>
2016-01-24 23:37:17 -0600

    Update the UnitTest.

--- src/provided/ParserTest.groovy
+++ src/provided/ParserTest.groovy
@@ -1,44 +1,113 @@
 package provided
 
+import com.apple.laf.AquaButtonBorder
 import junit.framework.TestCase
 import org.junit.Test
-import provided.ParseException;
-import provided.Parser;
-import junit.framework.*;
-import java.io.*;
+
 
 /**
  * Created by xiaozheng on 1/23/16.
  */
 public class ParserTest extends TestCase{
-    @Test
-    public void testParseFactor() throws Exception {
-        Parser parser = new Parser("src/provided/tests/simple/01good");
-        AST ast = parser.parse();
-        assertEquals(ast.toString(), "function?");
-        assertEquals(ast.class, PrimFun);
+
+    protected void checkString(String name, String answer, String program) {
+        Parser p = new Parser(new StringReader(program));
+        try{
+            assertEquals(name, answer, p.parse().toString());
+        } catch (ParseException e){
+            System.err.println(e);
+        }
     }
 
     @Test
-    public void testParseTerm() throws Exception {
-        Parser parser = new Parser("src/provided/tests/simple/00bad");
-        AST ast = parser.parse();
-        assertEquals(ast.toString(), "null");
-        assertEquals(ast.class, NullConstant);
+    public void testParseFactorPrim() throws Exception {
+        try {
+            String output = "function?";
+            String input = "function?";
+            checkString("prim  ", output, input );
+
+        } catch (Exception e) {
+            fail("prim   threw " + e);
+        }
+    }
+
+
+    @Test
+    public void testParseFactorId() throws Exception {
+        try {
+            String output = "car";
+            String input = "car";
+            checkString("id  ", output, input );
+
+        } catch (Exception e) {
+            fail("id   threw " + e);
+        }
     }
 
     @Test
-    public void testParseExp() throws Exception {
-        Parser parser = new Parser("src/provided/tests/in");
-        AST ast = parser.parse();
-        assertEquals(ast.toString(), "let");
+    public void testParseTermNull() throws Exception {
+        try {
+            String output = "null";
+            String input = "null";
+            checkString("nul  ", output, input );
+
+        } catch (Exception e) {
+            fail("null   threw " + e);
+        }
     }
 
     @Test
-    public void testParseIDList() throws Exception {
-        Parser parser = new Parser("src/provided/tests/in");
-        AST ast = parser.parse();
-        assertEquals(ast.class, KeyWord);
+    public void testParseExpLet() throws Exception {
+        try {
+            String output = "let a := 3; in (a + a)";
+            String input = "let a:=3; in a + a";
+            checkString("let", output, input );
+
+        } catch (Exception e) {
+            fail("let threw " + e);
+        }
     }
+
+
+    @Test
+    public void testParseExpMap() throws Exception {
+        try {
+            String output = "map f to (map x to f(x(x)))(map x to f(x(x)))";
+            String input = "map f to (map x to f( x( x ) ) ) (map x to f(x(x)))";
+            checkString("map", output, input );
+
+        } catch (Exception e) {
+            fail("map threw " + e);
+        }
+    }
+
+    @Test
+    public void testParseExpIf() throws Exception {
+        try {
+            String output = "if a = 3; then b = 2; else c = 3";
+            String input = "if a = 3; then b = 2; else c = 3";
+            checkString("if", output, input );
+
+        } catch (Exception e) {
+            fail("if threw " + e);
+        }
+    }
+
+    @Test
+    public void testParseException() {
+        try {
+            String output = "doh!";
+            String input = "map a, to 3";
+            checkString("parseException", output, input );
+
+            fail("parseException did not throw ParseException exception");
+            //} catch (ParseException e) {
+            //e.printStackTrace();
+
+        } catch (Exception e) {
+            fail("parseException threw " + e);
+        }
+    } //end of func
+
 
 }
