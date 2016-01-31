d03744feb357cbfdffc95d5389aa427aeef6d7f8
Xiao Zheng <yjzhengxiao@126.com>
2016-01-31 16:47:06 -0600

    Refactoring

--- Lab2/src/Interpreter.java
+++ Lab2/src/Interpreter.java
@@ -3,5 +3,18 @@
  */
 public class Interpreter {
 
-    
+    /** file Interpreter.java **/
+    class EvalException extends RuntimeException {
+        EvalException(String msg) { super(msg); }
+    }
+
+    class Interpreter {
+        Interpreter(String fileName) throws IOException;
+        Interpreter(Reader reader);
+
+        public JamVal callByValue() { ... };
+        public JamVal callByName()  { ... };
+        public JamVal callByNeed()  { ... };
+    }
+
 }
