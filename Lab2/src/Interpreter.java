516c47e4a013c5fb83122c68f9304f0466fcf708
Xiao Zheng <yjzhengxiao@126.com>
2016-02-08 04:38:35 -0600

    Adding Code

--- Lab2/src/Interpreter.java
+++ Lab2/src/Interpreter.java
@@ -44,32 +44,229 @@ public class Interpreter {
 			};
 			@Override
     		public JamVal forIntConstant(IntConstant i){
-    			  return i;
-    		  }
+				final IntConstant ii = i;
+				JamVal result = new JamVal() {
+					@Override
+					public <JamVal> JamVal accept(JamValVisitor<JamVal> jvv) {
+						return ii.accept(jvv);
+					}
+				};
+				return result;
+			}
 			@Override
     		public JamVal forNullConstant(NullConstant n){
-    			return n;
+				return (JamVal) JamEmpty.ONLY;
 			}
 			@Override
     		public JamVal forJamEmpty(JamEmpty je){
-    			  return je;
-    		  }
+				return (JamVal) JamEmpty.ONLY;
+			}
 			@Override
 			public JamVal forVariable(Variable v){
     			  return v;
-    		  }
+    		}
 			@Override
 			public JamVal forPrimFun(PrimFun f){
     			  return f;
-    		  }
+    		}
 			@Override
 			public JamVal forUnOpApp(UnOpApp u){
-    			  return u;
-    		  }
+				final UnOp op = u.rator();
+				nextAST = u.arg();
+				JamVal argVal = callByValue();
+				argVal.accept(new JamValVisitor<Boolean>() {
+					@Override
+					public Boolean forBoolConstant(BoolConstant jb) {
+						if (op.name == "~"){
+							if (jb.value())
+								return false;
+							else
+								return true;
+						} else{
+							throw new EvalException("unop got " + op.toString() + " on a boolean");
+						}
+					}
+
+					@Override
+					public Boolean forIntConstant(IntConstant ji) {
+						if (op.name == "+"){
+							return (ji.value() > 0);
+						} else if (op.name == "-"){
+							return (ji.value() < 0);
+						} else {
+							throw new EvalException("unop got " + op.toString() + "on an int");
+						}
+					}
+
+					@Override
+					public Boolean forJamFun(JamFun jf) {
+						throw new EvalException("unop was given fun " + jf.toString());
+					}
+
+					@Override
+					public Boolean forJamList(JamList jl) {
+						throw new EvalException("unop was given list " + jl.toString());
+					}
+				});
+
+
+    		}
 			@Override
 			public JamVal forBinOpApp(BinOpApp b){
-    			  return b;
-    		  }
+    			final BinOp op = b.rator();
+				nextAST = b.arg1();
+				final JamVal arg1Val = callByValue();
+				nextAST = b.arg2();
+				final JamVal arg2Val = callByValue(){
+				}
+				switch (op.name) {
+					case "+":
+					case "-":
+					case "*":
+					case "/":
+						arg1Val.accept(new JamValVisitor<IntConstant>() {
+							@Override
+							public IntConstant forBoolConstant(BoolConstant jb) {
+								throw new EvalException("binop " + op.toString() + " was given bool " + jb.toString());
+							}
+
+							@Override
+							public IntConstant forIntConstant(IntConstant ji) {
+								int a = ji.value();
+								int b = ((IntConstant) arg2Val).value();
+								if (op.name == "+") {
+									return new IntConstant(a + b);
+								} else if (op.name == "-") {
+									return new IntConstant(a - b);
+								} else if (op.name == "*") {
+									return new IntConstant(a * b);
+								} else if (op.name == "/") {
+									if (b == 0) {
+										throw new EvalException("divide by zero");
+									}
+									return new IntConstant(a / b);
+								}
+							}
+
+							@Override
+							public IntConstant forJamFun(JamFun jf) {
+								throw new EvalException("binop " + op.toString() + " was given fun " + jf.toString());
+							}
+
+							@Override
+							public IntConstant forJamList(JamList jl) {
+								throw new EvalException("binop " + op.toString() + " was given list " + jl.toString());
+							}
+						});
+						break;
+					case "=":
+					case "!=":
+					case "<":
+					case "<=":
+					case ">":
+					case ">=":
+					case "&":
+					case "|":
+						arg1Val.accept(new JamValVisitor<BoolConstant>() {
+							@Override
+							public BoolConstant forBoolConstant(BoolConstant jb) {
+								Boolean a = jb.value();
+								Boolean b = ((BoolConstant) arg2Val).value();
+								if (op.name == "="){
+									if (a == b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								}else if (op.name == "!="){
+									if (a != b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								}else if (op.name == "&"){
+									if (a && b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								}else if (op.name == "|"){
+									if (a || b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								} else{
+									throw new EvalException("binop " + op.toString() + " was given bool " + jb.toString());
+								}
+							}
+
+							@Override
+							public BoolConstant forIntConstant(IntConstant ji) {
+								int a = ji.value();
+								int b = ((IntConstant) arg2Val).value();
+								if (op.name == "="){
+									if (a == b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								}else if (op.name == "!=") {
+									if (a != b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								}else if (op.name == "<") {
+									if (a < b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								}else if (op.name == "<=") {
+									if (a <= b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								}else if (op.name == ">") {
+									if (a > b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								}else if (op.name == ">=") {
+									if (a >= b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								}else{
+									throw new EvalException("binop " + op.toString() + " was given int " + ji.toString());
+								}
+							}
+							@Override
+							public BoolConstant forJamFun(JamFun jf) {
+								int a = jf.hashCode();
+								int b = ((JamFun) arg2Val).hashCode();
+								if (op.name == "="){
+									if (a == b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								} else {
+									throw new EvalException("binop " + op.toString() + " was given fun " + jf.toString());
+								}
+
+							}
+
+							@Override
+							public BoolConstant forJamList(JamList jl) {
+								int a = jl.hashCode();
+								int b = ((JamList) arg2Val).hashCode();
+								if (op.name == "="){
+									if (a == b)
+										return BoolConstant.TRUE;
+									else
+										return BoolConstant.FALSE;
+								} else {
+									throw new EvalException("binop " + op.toString() + " was given list " + jl.toString());
+								}
+
+							}
+						});
+				}
+			}
 			@Override
     		public JamVal forApp(App a){
     			  return a;
